3e459dedd110a271dd70b298874c14790249a0ec
devanaclimgo <anaclimgo@gmail.com>
2024-09-12 11:37:46 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-// hello world
-let mensagem = 2;
-console.log("hello world!");
-console.log(mensagem);
-
-
-// arrays, objetos
-let metas = ["ana", "alô"]
-
-console.log(metas[1] + " " + metas[0]);
-
 let meta = {
   value: "ler um livro por mês",
   checked: false,
-  isChecked: (info) => {
-    console.log(info)
-  }
 }
 
-meta.value = "assistir um filme todo final de semana"
-meta.isChecked(meta.value)
+let metas = [
+  meta,
+  {
+    value: "estudar programação todo dia",
+    checked: true
+  }
+]
 
+console.log(metas[1].value)
 
-// function // arrow function
-const criarMeta = () => {}
-
-//function criarMeta() {}
+//(meta.vale) = (meta[0].value)
