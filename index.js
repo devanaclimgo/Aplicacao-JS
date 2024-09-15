b835323b4db5d4d0748b9d2425483bbf4bd899ad
devanaclimgo <anaclimgo@gmail.com>
2024-09-15 19:09:20 -0300

    deletar metas

--- index.js
+++ index.js
@@ -73,9 +73,34 @@ const metasAbertas = async () => {
   }
 
   await select({
-    message: "Metas abertas" + abertas.length,
+    message: "Metas abertas:" + abertas.length,
     choices: [...abertas],
   })
+}
+
+const deletarMetas = async () => {
+  const metasDesmarcadas = metas.map((meta) => {
+    return {value: meta.value, checked: false}
+  })
+  const itensADeletar = await checkbox({
+    message:
+      "Selecione item para deletar",
+    choices: [...metasDesmarcadas],
+    instructions: false,
+  })
+
+  if(itensADeletar.length == 0) {
+    console.log("Nenhum item para deletar!")
+    return
+  }
+
+  itensADeletar.forEach((item) => {
+    metas = metas.filter((meta) => {
+      return meta.value != item
+    })
+  })
+
+  console.log("Meta(s) deletada(s) com sucesso!")
 }
 
 const start = async () => {
@@ -100,6 +125,10 @@ const start = async () => {
           value: "abertas",
         },
         {
+          name: "Deletar metas",
+          value: "deletar",
+        },
+        {
           name: "Sair",
           value: "sair",
         },
@@ -120,6 +149,9 @@ const start = async () => {
       case "abertas":
         await metasAbertas();
         break;
+        case "deletar":
+          await deletarMetas();
+          break;
       case "sair":
         console.log("Até a próxima!");
         return;
