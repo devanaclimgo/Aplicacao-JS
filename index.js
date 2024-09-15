c6e53abb1c907a21e02652c8fb8c60481809fe54
devanaclimgo <anaclimgo@gmail.com>
2024-09-15 18:56:17 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -48,17 +48,33 @@ const listarMetas = async () => {
 
 const metasRealizadas = async () => {
   const realizadas = metas.filter((meta) => {
-    return meta.checked
-  })
+    return meta.checked;
+  });
 
-  if(realizadas.length == 0) {
-    console.log("Não existem metas realizadas! :(")
-      return
+  if (realizadas.length == 0) {
+    console.log("Não existem metas realizadas! :(");
+    return;
   }
 
   await select({
-    message: "Metas realizadas",
-    choices: [...realizadas]
+    message: "Metas realizadas" + realizadas.length,
+    choices: [...realizadas],
+  });
+};
+// agua [] - caminhar [] - cantar [x]
+const metasAbertas = async () => {
+  const abertas = metas.filter((meta) => {
+    return !meta.checked
+  })
+
+  if(abertas.length == 0) {
+    console.log("Não existem metas abertas! :)")
+    return
+  }
+
+  await select({
+    message: "Metas abertas" + abertas.length,
+    choices: [...abertas],
   })
 }
 
@@ -80,6 +96,10 @@ const start = async () => {
           value: "realizadas",
         },
         {
+          name: "Metas abertas",
+          value: "abertas",
+        },
+        {
           name: "Sair",
           value: "sair",
         },
@@ -96,6 +116,9 @@ const start = async () => {
         break;
       case "realizadas":
         await metasRealizadas();
+        break;
+      case "abertas":
+        await metasAbertas();
         break;
       case "sair":
         console.log("Até a próxima!");
